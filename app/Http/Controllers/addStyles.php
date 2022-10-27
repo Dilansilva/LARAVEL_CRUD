06cd6c577dd85941c0fca60aad34b88e5b6c3b1e
dilan <dilan.silva@colombo.rezgateway.com>
2022-10-27 22:24:09 +0530

    Initial commit

---
+++ app/Http/Controllers/addStyles.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class addStyles extends Controller
+{
+    
+    function addData(){
+        return ["name"=>"DilanSilva"];
+    }
+}
